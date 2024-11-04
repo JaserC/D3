3eecb2495d4fdf141e0d0679cd2e0482b714f14d
Jaser Chowdhury <jaserchowdhury@gmail.com>
2024-11-03 22:43:36 -0800

    border changes

--- script.js
+++ script.js
@@ -8,70 +8,76 @@ let dataUrl = 'data/election-data.csv'
 let div = d3.select('body').append('div')
             .attr('class', 'tooltip')
 
-function numFormat(num) {
+const zoom = d3.zoom()
+  .scaleExtent([1, 8]) // Limit the zoom scale, adjust as needed
+  .on("zoom", (event) => {
+      g.attr("transform", event.transform); // Apply zoom transformation to the map group
+});
+
+function numFormat (num) {
+    // Round and add comma to numbers for display
   return (+num).toLocaleString()
 }
-
-// Define zoom behavior
-const zoom = d3.zoom()
-    .scaleExtent([1, 8]) // Limit the zoom scale, adjust as needed
-    .on("zoom", (event) => {
-        g.attr("transform", event.transform); // Apply zoom transformation to the map group
-    });
 
 d3.queue()
     .defer(d3.json, mapUrl)
     .defer(d3.csv, dataUrl, (d) => {
+        // Add data to map for mapping to topojson in the future
       map.set(d.county_fips, {
         winPercent: d.per_gop - d.per_dem,
         countyName: d.county_name,
         votesDem: d.votes_dem,
         votesGop: d.votes_gop,
         votesTotal: d.total_votes
-      });
-    })
-    .await((error, us) => {
-      if (error) throw Error(error);
-
-      // Append a group element to hold all the map paths, allowing us to zoom and pan the map
-      const g = svg.append("g");
-
-      g.append('g')
+      })
+    }).await((error, us) => {
+      if (error) throw Error(error)
+      svg.append('g')
             .attr('class', 'counties')
             .selectAll('path')
             .data(topojson.feature(us, us.objects.counties).features)
             .enter().append('path')
             .attr('fill', (d) => {
-              let dataPoint = map.get(parseInt(d.id));
-              dataPoint = dataPoint ? dataPoint.winPercent : 0.05;
+              let dataPoint = map.get(parseInt(d.id))
+              dataPoint = dataPoint ? dataPoint.winPercent : 0.05
 
-              if (dataPoint >= 0.05) return '#f44336';
-              if (dataPoint > 0) return '#ef9a9a';
-              if (dataPoint >= -0.05) return '#bbdefb';
-              return '#2196f3';
+                // Base county colors on win % per county
+              if (dataPoint >= 0.05) return '#f44336'
+              if (dataPoint > 0) return '#ef9a9a'
+              if (dataPoint >= -0.05) return '#bbdefb'
+              return '#2196f3'
             })
             .attr('d', path)
             .on('mouseover', (d) => {
-              d = map.get(parseInt(d.id));
+                // add tooltip on hover with information about the race
+              d = map.get(parseInt(d.id))
               div.transition()
-                .duration(200)
-                .style('opacity', 0.9);
+                    .duration(200)
+                    .style('opacity', 0.9)
 
               div.html(
-                `<b>County:</b> ${d.countyName}
-                <br/>
-                <b>Trump:</b> ${numFormat(d.votesGop)}
-                <br/>
-                <b>Biden:</b> ${numFormat(d.votesDem)}
-                <br/>
-                <b>Total:</b> ${numFormat(d.votesTotal)}`
-              )
-              .style('left', (d3.event.pageX + 15) + 'px')
-              .style('top', (d3.event.pageY - 28) + 'px');
-            });
+                        `<b>County:</b> ${d.countyName}
+                        <br/>
+                        <b>Trump:</b> ${numFormat(d.votesGop)}
+                        <br/>
+                        <b>Biden:</b> ${numFormat(d.votesDem)}
+                        <br/>
+                        <b>Total:</b> ${numFormat(d.votesTotal)}`
+                    )
+                    .style('left', (d3.event.pageX + 15) + 'px')
+                    .style('top', (d3.event.pageY - 28) + 'px')
+            })
 
-      // Draw county borders
-      g.append('path')
+        // Fill in the county outlines
+    //   svg.append('path')
+    //         .datum(topojson.meshus, us.objects.states, (a, b) => {
+    //           return a !== b
+    //         })
+    //         .attr('class', 'states')
+    //         .attr('d', path)
+    // })
+
+    svg.append('path')
          .datum(topojson.mesh(us, us.objects.counties, (a, b) => a !== b))
          .attr('class', 'county-borders')
          .attr('d', path)
@@ -79,15 +85,12 @@ d3.queue()
          .attr('stroke', '#FFFFFF')
          .attr('stroke-width', 0.2);
 
-      // Draw state borders
-      g.append('path')
+      // Draw state outlines on top of county borders
+      svg.append('path')
          .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
          .attr('class', 'state-borders')
          .attr('d', path)
          .attr('fill', 'none')
-         .attr('stroke', '#9c9898')
-         .attr('stroke-width', 1.0);
-
-      // Apply the zoom behavior to the SVG element
-      svg.call(zoom);
+         .attr('stroke', '#FFFFFF')  
+         .attr('stroke-width', 4);
     });
